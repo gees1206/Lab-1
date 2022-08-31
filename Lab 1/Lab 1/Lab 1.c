8b033f324b3ecf708cf0fa1a78f5078d4312703f
szabgab19 <64235603+szabgab19@users.noreply.github.com>
2022-08-30 19:57:52 -0700

    Initial code

--- Lab 1/Lab 1/Lab 1.c
+++ Lab 1/Lab 1/Lab 1.c
@@ -1,20 +1,53 @@
-// Lab 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*******************************************************************************************
+
+* Programmer: Gabriel Szabo
+
+* Class: CptS 121, Fall 2022; Lab Section 15
+
+* Programming Assignment: Lab 1
+
+* Date: August 30, 2022
+
+* Description: This program evaluates simple equations provided in PA1.
+
+*******************************************************************************************/
 
 #include <iostream>
 
-int main()
+#define _CRT_SECURE_NO_WARNINGS // necessary to ignore scanf_s () warnings
+
+
+
+#include <stdio.h> // necessary to use printf () and scanf ()
+
+
+
+int main(void) // the starting point for all C programs
+
 {
-    std::cout << "Hello World!\n";
-}
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    // we need to request memory for
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    int number1_int = 0, number2_int = 0; // 2 variable declarations - reserves two memory blocks for integers and sets them to 0's
+
+    double number1_float = 0.0, number2_float = 0.0; // reserves two memory blocks for numbers with high precision (floating-point)
+
+
+
+    printf("Enter two integer values: "); // ask the user
+
+    scanf("%d%d", &number1_int, &number2_int); // read the integers typed through the keyboard - %d is for "decimal", i.e. integers
+
+
+
+    printf("Enter two floating-point values: "); // ask the user
+
+    scanf("%lf%lf", &number1_float, &number2_float); // read the integers typed through the keyboard - %lf is for "long float", i.e. double precision numbers
+
+
+
+    // place all code for the subtasks/operations below here
+
+    return 0; // return a success status (value 0) indicating the program worked fine
+
+} // end of the main () function
