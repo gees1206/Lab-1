ac6f1c97b5b4b6eb9d084d23ebf4dc683c135bb7
gees1206 <112530630+gees1206@users.noreply.github.com>
2022-09-23 16:57:00 -0700

    3

---
+++ Lab 4/Lab 4/handler.h
@@ -0,0 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS // necessary to ignore scanf_s () warnings
+#include <stdio.h> // necessary to use printf () and scanf ()
+#include <math.h> // for sqrt()
+#define G 32.17 //Define macro constant for G
