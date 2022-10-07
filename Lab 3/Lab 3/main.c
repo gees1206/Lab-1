390bdf01ada9e820bda0fc52c0a0b98a17ed4ddf
szabgab19 <gabriel.szabo1206@gmail.com>
2022-10-07 12:02:56 -0700

    Find largest num

--- Lab 3/Lab 3/main.c
+++ Lab 3/Lab 3/main.c
@@ -3,6 +3,20 @@
 
 
 int main(void) {
+
+	int num = 583;
+	int largest = 0;
+	int x = 0;
+
+	while (num != 0){
+		x = num % 10;
+		largest = fmax(x,largest);
+		num = num / 10;
+	}
+	
+
+	printf("%d", largest);
+	/*
 	double theta, distance, velocity;
 		
 	theta = get_theta();
@@ -13,6 +27,7 @@ int main(void) {
 	double height = calc_height(distance,velocity,theta);
 	printf("Flight time: %lf", time);
 	printf("\nHeight: %lf", height);
+	*/
 	
 	return 0;
 }
